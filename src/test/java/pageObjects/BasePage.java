b316cd00c260c1d398e3a4a1eeecbc6284b7a018
Vyshak <vyshakma7020@gmail.com>
2026-01-12 00:12:45 +0530

    Updating BasePage.java

--- src/test/java/pageObjects/BasePage.java
+++ src/test/java/pageObjects/BasePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.PageFactory;
 public class BasePage {
 	
 	WebDriver driver;
-	
+	//test
 	public BasePage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver,this);
